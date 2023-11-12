fe6a9de1ebf0b6ad5023b58e2f9d6bdf7a8265f8
Camilo Figueroa <cigotete@gmail.com>
2023-11-12 17:05:37 -0500

    Updating App with Redux state.

--- src/BooksListApp.jsx
+++ src/BooksListApp.jsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchBooks } from "./store/slices/books";
+import { Space, Typography, List, Button } from 'antd';
+
+const { Text, Title } = Typography;
 
 export const BooksListApp = () => {
 
   const dispatch = useDispatch();
+  const { books = [], page, isLoading } = useSelector(state => state.booksList);
 
   useEffect(() => {
     dispatch(fetchBooks());
@@ -12,7 +16,22 @@ export const BooksListApp = () => {
 
   return (
     <>
-      qwe
+      <Space direction="vertical">
+      <Title>Books List</Title>
+      <Text>Loading { isLoading ? 'true' : 'false' } </Text>
+      <Button disabled={ isLoading } type="primary" onClick={ () => dispatch(fetchBooks(page)) }>
+        Next</Button>
+      <List
+        bordered
+        dataSource={ books }
+        renderItem={(item) => (
+          <List.Item>
+            <Typography.Text>{item.title}</Typography.Text>
+          </List.Item>
+        )}
+      />
+
+      </Space>
     </>
   );
 }
--- src/store/slices/books/booksListSlice.js
+++ src/store/slices/books/booksListSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const booksListSlice = createSlice({
     name: 'booksList',
     initialState: {
-        page: 0,
+        page: 1,
         books: [],
         isLoading: false,
     },
