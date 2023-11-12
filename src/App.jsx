d4af338f66180c9ca0fe25337cf45f351f050f8d
Camilo Figueroa <cigotete@gmail.com>
2023-11-11 22:21:38 -0500

    Basics Redux n Redux Toolkit. Implemented a basic counter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useSelector, useDispatch } from "react-redux"
+import { increment } from './store/slices/counter'
 
 export function App() {
 
+  const { counter } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch( increment() )
+  }
+
   return (
     <>
-    Hello world!
+      { counter }
+      <button type="button" onClick={ handleClick }>
+        Increment
+      </button>
     </>
   )
 }
