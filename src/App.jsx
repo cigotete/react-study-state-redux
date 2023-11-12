ddb2ea3d7a1af61b38a23bd151eddab6c1ad3526
Camilo Figueroa <cigotete@gmail.com>
2023-11-11 22:24:42 -0500

    Basics Redux n RToolkit. An Increment, decrement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useSelector, useDispatch } from "react-redux"
-import { increment } from './store/slices/counter'
+import { increment, incrementBy, decrement } from './store/slices/counter'
+import { Button, Space, Typography } from 'antd'
+
+const { Text } = Typography;
 
 export function App() {
 
   const { counter } = useSelector(state => state.counter);
   const dispatch = useDispatch();
 
-  const handleClick = () => {
-    dispatch( increment() )
+  const handleIncrement = () => {
+    dispatch(increment());
+  }
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  }
+
+  const handleIncrementBy = (amount) => {
+    dispatch(incrementBy(amount));
   }
 
   return (
     <>
-      { counter }
-      <button type="button" onClick={ handleClick }>
+      <Space direction="vertical">
+      <Text>{ counter }</Text>
+      <Button type="primary" onClick={ handleIncrement }>
         Increment
-      </button>
+      </Button>
+      <Button type="primary" onClick={ handleDecrement }>
+        Decrement
+      </Button>
+      <Button type="primary" onClick={ () => handleIncrementBy(2) }>
+        Increment By
+      </Button>
+      </Space>
     </>
   )
 }
